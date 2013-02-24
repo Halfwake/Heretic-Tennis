00caed0be4d5dba01c80fb5f049eef7e1e3b8f1d
Drew Dudash <drewdudash@gmail.com>
2013-02-24 15:56:37 -0600

    Used a better member selection operator for create_paddle

--- paddle.c
+++ paddle.c
@@ -11,13 +11,13 @@ Paddle *create_paddle(int x, int y, bool player) {
 		Paddle *p = (Paddle*) malloc(sizeof(Paddle));
 		
 		SDL_Rect *pad_rect = (SDL_Rect*) malloc(sizeof(SDL_Rect));
-		(*pad_rect).x = x;
-		(*pad_rect).y = y;
-		(*pad_rect).w = default_paddle_width;
-		(*pad_rect).h = default_paddle_height;
-		(*p).rect = pad_rect;
+		pad_rect->x = x;
+		pad_rect->y = y;
+		pad_rect->w = default_paddle_width;
+		pad_rect->h = default_paddle_height;
+		p->rect = pad_rect;
 		
-		(*p).player = player;
+		p->player = player;
 		return p;
 }
 
