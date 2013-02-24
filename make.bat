59cdff204a7a022f304d4004992ce3372ca4036d
Drew Dudash <drewdudash@gmail.com>
2013-02-23 21:21:08 -0600

    Fixed the make file to work with the new project name.

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-gcc fourth_reich.c paddle.c -o FourthReich -L/usr/local/cross-tools/i686-w64-mingw32/lib -lmingw32 -mwindows -L./ -lSDL -lSDLmain
+gcc heretic_tennis.c paddle.c -o HereticTennis -L/usr/local/cross-tools/i686-w64-mingw32/lib -lmingw32 -mwindows -L./ -lSDL -lSDLmain
